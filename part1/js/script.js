ef7d4b0dbf1c7fb6d588b6251940811a2f8989dd
qbscarlie <qbscarlie@gmail.com>
2016-04-12 19:31:13 -0500

    First commit

--- part1/js/script.js
+++ part1/js/script.js
@@ -9,49 +9,140 @@
 
 
 // DEFINE CHART DIMENSIONS
+var margin = {top: 20, right: 60, bottom: 30, left: 50},
+    width = 920 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 
 
 // DEFINE SCALES
+var x = d3.scale.linear()
+    .range([0, width]);
 
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var color = d3.scale.ordinal()
+    .domain(['< 10%','10-20%','> 20%'])
+    .range(['#fa9fb5','#dd3497','#7a0177']);
 
 
 // DEFINE AXES
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+    .tickFormat(function(d){
+        return d+"%"
+    });
 
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
 
 
 // APPEND SVG TO THE DOM
-
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
 // IMPORT DATA
-
+d3.tsv("data/sat.tsv", function(error, data) {
 
 
 // PREPARE DATA
-
+  data.forEach(function(d) {
+    d.state = d["State"];
+    d.tested = +d["Tested"];
+    d.score = +d["Score"];
+    d.waiver = d["Waiver"];
+  });
 
 
 // SET SCALES DOMAIN
+  x.domain([0, 100]);
+  y.domain(d3.extent(data, function(d) { return d.score; })).nice();
 
+
+  var tooltip = d3.select("#tooltip");
 
 
 // APPEND AXES
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("x", width) 
+      .attr("y", -6)
+      .style("text-anchor", "end") 
+      .text("Student participation rate");
 
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("transform", "rotate(-90)") 
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Average SAT score")
 
 
 // DOTS
 // 1) SELECT
-
+      var dots = svg.selectAll(".dot")
 
 
 // 2) JOIN
-
+            .data(data);
 
 
 // 3) ENTER
-
+      dots.enter()
+            .append("circle")
+            .attr("class", "dot");
 
 
 
 // 4) UPDATE
+      dots
+        .attr("r", 5)
+        .attr("cx",function(d){ return x(d.tested); })
+        .attr("cy",function(d){ return y(d.score); })
+        .style("stroke","white")
+        .style("stroke-width","1px")
+        .style("fill", function(d){
+            return color(d.waiver);
+        })
+        .on("mouseover",function(d){
+
+            tooltip
+                .style("left", (d3.event.pageX + 10 ) + "px")
+                .style("top" , (d3.event.pageY - 30 - document.body.scrollTop ) + "px");
+            tooltip.classed("hidden",false);
+
+            tooltip.select("p")
+                .text(d.state);
+        })
+        .on("mouseout", function(d){
+            tooltip.classed("hidden",true);
+        });
+
+//ADD LEGEND
+        var keys = d3.select("#key-table").selectAll(".key")
+            .data(color.domain()) 
+            .enter()
+            .append("div") 
+            .attr("class","key")
+            .html(function(d){
+                return "<i class='fa fa-circle' style='color:"
+                +color(d)+
+                ";'></i>"+d;});
+
+
+});
--- part2/js/script.js
+++ part2/js/script.js
@@ -1,50 +1,105 @@
 // DEFINE CHART DIMENSIONS
-
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 650 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 
 // DEFINE SCALES
-
+var x = d3.scale.ordinal()
+	.rangeRoundBands([0, width], .1);
+var y = d3.scale.linear()
+    .range([height, 0]);
 
 
 // DEFINE AXES
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
 
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10, "%");
 
 
 // APPEND SVG AND AXES GROUPS TO THE DOM
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")");
+
+svg.append("g")
+      .attr("class", "y axis");
 
 
 
 // IMPORT DATA
-
+var chartData;
+d3.tsv("data/set2.tsv", function(error, data) {
 
 
 // PREPARE DATA
+  data.forEach(function(d){
+    d.value = +d.value;
+  });
+  chartData = d3.nest()
+                .key(function(d){ return d.group; })
+                .map(data);
 
+  draw('A');
+
+});
 
 // WRITE DRAW FUNCTION
-
+function draw(group){
+  var data = chartData[group];
 
 
 // SET SCALES DOMAIN
-
+  x.domain(data.map(function(d) { return d.label; }));
+  y.domain([0, d3.max(data, function(d) { return d.value; })]).nice();
 
 
 // CALL AXES
+  d3.selectAll(".x.axis")
+      .call(xAxis);
 
+  d3.selectAll(".y.axis")
+      .call(yAxis);
 
 
 // BARS
 // 1) JOIN
-
+  var bars = svg.selectAll(".bar")
+      .data(data);
 
 
 // 2) ENTER
-
+  bars
+    .enter().append("rect")
+      .attr("class", "bar");
 
 
 
 // 3) UPDATE
-
+  bars
+      .attr("width", x.rangeBand())
+      .attr("x", function(d) { return x(d.label); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return height - y(d.value); });
 
 
 // 4) EXIT
+  bars.exit().remove();
+
+}
+
+
+// Call our draw function using our buttons
+$(".btn").click(function(){
+  draw($(this).text());
+});
